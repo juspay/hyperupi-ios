7413972e026fc7dfbc35d1a6f68e9d39304bb61d
Balaganesh S <balaganesh@juspay.in>
2026-07-17 18:44:01 +0530

    Updated Package for version 2.2.8.3

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperUPI",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.2/HyperUPI.zip",
-            checksum: "9c31d8ecd111fe64d143464e31b8ed171db730ca02d09991690bf3f87cb88d6c"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.3/HyperUPI.zip",
+            checksum: "0b295bb6865eb828922dbeebe1a6506cc6a20ba3dafa8fae46723d4ce3e5ebc1"
         )
     ]
 )
